ee1110e164d844da23d14e11128fc780e3d5c186
Daniel Parks <dparks715@yahoo.com>
2017-02-14 02:22:23 -0500

    Math seems to be in order, will add more tests

--- coins.rb
+++ coins.rb
@@ -3,15 +3,14 @@ def coin_changer(change)
 	your_change = {quarters: 0, dimes: 0, nickels: 0, pennies: 0}
 	# Hash representing the value of each coin.
 	coin_value = {quarters: 25, dimes: 10, nickels: 5, pennies: 1}
-	sum = 0
+	
 
 	# First thought is to iterate through coin_value
 	# Somehow check how many of each item will fit in my param (change)
 	# Then take how many and alter the value of your_change.
-	coin_value.each { |key, value| while sum < change
+	coin_value.each { |key, value| while value <= change
 										your_change[key] += 1
-										sum += value
-										
+										change = change - value
 									end}
 
 	your_change # Hash to be returned with values altered to represent number of each coin.
