c4494200eb2e53bd514fc08b44eee458fde950c3
Daniel Parks <dparks715@yahoo.com>
2017-02-14 01:34:38 -0500

    Setting up function to compare each value to my parameter

--- coins.rb
+++ coins.rb
@@ -1,3 +1,12 @@
-def coin_changer()
-	coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+def coin_changer(change)
+	# Hash with coins we want returned, default 0, will add to this and return it at the end.
+	your_change = {quarters: 0, dimes: 0, nickels: 0, pennies: 0}
+	# Hash representing the value of each coin.
+	coin_value = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+
+	# First thought is to iterate through coin_value
+	# Somehow check how many of each item will fit in my param (change)
+	# Then take how many and alter the value of your_change.
+	coin_value.each_value
+	your_change # Hash to be returned with values altered to represent number of each coin.
 end
