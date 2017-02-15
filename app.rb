e03b91d10f7498287e8b00ad37cba41c0323b389
Daniel Parks <dparks715@yahoo.com>
2017-02-15 06:52:05 -0500

    Fixed sinatra problem, needed to convert to_i in app file

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require 'sinatra'
 require_relative 'coins.rb'
 
 get '/' do
-	erb :change_input, :locals => {results: '', coins: ''}
+	erb :change_input, :locals => {results: '', message1: ''}
 end
 
 post '/coins' do
-	coins = params[:change]
+	coins = params[:change].to_i
 	results = coins_to_string(coins)
-	erb :change_input, :locals => {results: results, coins: coins}
+	erb :change_input, :locals => {results: results, message1: 'Your change is: '}
 
 end
